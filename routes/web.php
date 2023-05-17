5b625d6a4c28f6ea97321fa8e682866c6d850a0e
maheswar behera <bmaheswar439@gmail.com>
2023-05-17 12:28:16 +0530

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\EmployeesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,14 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/employees',[EmployeesController::class,'index'])->name('employees.index');
-Route::get('/employees/create',[EmployeesController::class,'create'])->name('employees.create');
-Route::post('/employees',[EmployeesControlle::class,'store'])->name('employees.store');
